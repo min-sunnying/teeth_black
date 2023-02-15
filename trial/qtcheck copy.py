7806a0da2ff997698d4612978d68bc545fd3d870
Ubuntu <9909cindy@kaist.ac.kr>
2023-02-15 14:30:48 +0900

    total

--- trial/qtcheck copy.py
+++ trial/qtcheck copy.py
@@ -1,72 +1,22 @@
-from typing import List, Tuple
-from collections import deque
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from matplotlib import style
 
-def largestRegion(grid: List[List[int]]) -> int:
-	m = len(grid)
-	n = len(grid[0])
+plt.figure('SPLTV', figsize=(10, 5))
+custom=plt.subplot(121, projection='3d')
+x1=np.array([1, -2, 1])
+y1=np.array([5, 3, 7])
+z1=np.array([0, 0, 6])
 
-	# creating a queue that will help in bfs traversal
-	q = deque()
-	area = 0
-	ans = 0
-	for i in range(m):
-		for j in range(n):
-			# if the value at any particular cell is 1 then
-			# from here we need to do the BFS traversal
-			if grid[i][j] == 1:
-				ans = 0
-				# pushing the pair(i,j) in the queue
-				q.append((i, j))
-				# marking the value 1 to -1 so that we
-				# don't again push this cell in the queue
-				grid[i][j] = -1
-				while len(q) > 0:
-					t = q.popleft()
-					ans += 1
-					x, y = t[0], t[1]
-					# now we will check in all 8 directions
-					if x + 1 < m:
-						if grid[x + 1][y] == 1:
-							q.append((x + 1, y))
-							grid[x + 1][y] = -1
-					if x - 1 >= 0:
-						if grid[x - 1][y] == 1:
-							q.append((x - 1, y))
-							grid[x - 1][y] = -1
-					if y + 1 < n:
-						if grid[x][y + 1] == 1:
-							q.append((x, y + 1))
-							grid[x][y + 1] = -1
-					if y - 1 >= 0:
-						if grid[x][y - 1] == 1:
-							q.append((x, y - 1))
-							grid[x][y - 1] = -1
-					if x + 1 < m and y + 1 < n:
-						if grid[x + 1][y + 1] == 1:
-							q.append((x + 1, y + 1))
-							grid[x + 1][y + 1] = -1
-					if x - 1 >= 0 and y + 1 < n:
-						if grid[x - 1][y + 1] == 1:
-							q.append((x - 1, y + 1))
-							grid[x - 1][y + 1] = -1
-					if x - 1 >= 0 and y - 1 >= 0:
-						if grid[x - 1][y - 1] == 1:
-							q.append((x - 1, y - 1))
-							grid[x - 1][y - 1] = -1
-					if x + 1 < m and y - 1 >= 0:
-						if grid[x + 1][y - 1] == 1:
-							q.append((x + 1, y - 1))
-							grid[x + 1][y - 1] = -1
-				area = max(area, ans)
-	return area
+custom.scatter(x1, y1, z1)
 
-def main():
-    grid = [[1,1,1,1,1,1,1],
-            [1,1,0,0,0,1,1],
-            [1,1,0,1,0,1,1],
-            [1,1,0,0,0,1,1],
-            [1,1,1,1,1,1,1]]
-    result = largestRegion(grid)
-    print(f'Largest region of 1s has an area of {result}')
+verts=[list(zip(x1, y1, z1))]
+srf=Poly3DCollection(verts, alpha=.25, facecolor='#800000')
+plt.gca.add_collection3d(srf)
 
-main()
+custom.set_xlabel('X')
+custom.set_ylabel('Y')
+custom.set_zlabel('Z')
+plt.show()
